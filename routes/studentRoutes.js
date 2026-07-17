9ebe8dfe2e272f430792fa0cd8d07cde590be00b
minhajahmdtk <minhajahammedtk@gmail.com>
2026-07-17 10:27:14 +0530

    first commit

---
+++ routes/studentRoutes.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const router=express.Router();
+
+let student=[];
+router.post('/create',(req,res)=>{
+  
+})
+ 
+
+module.exports=router;
---
+++ server.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const server=express();
+const port=3000;
+const studentRoutes=require('./routes/studentRoutes');
+server.use(express.json());
+server.use('/student',studentRoutes);
+
+server.listen(port,()=>{
+  console.log(`server is running on port ${port}`);
+})
